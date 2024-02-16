4ac1878e60b12564d09e5f5483f308f5f76bbd37
Milton Girón <46804135+miltong261@users.noreply.github.com>
2024-02-16 00:12:57 -0600

    Extras
    * Functionality to change state
    * Pagination
    * Button return back to tasks

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -3,6 +3,8 @@
 @section('title', $task->title)
 
 @section('content')
+    <a href="{{ route('tasks.index') }}">Regresar</a>
+
     <p>{{ $task->description }}</p>
 
     @if ($task->long_description)
@@ -11,6 +13,12 @@
 
     <p>{{ $task->created_at }}</p>
     <p>{{ $task->updated_at }}</p>
+
+    <form action="{{ route('tasks.toggle-complete', ['task' => $task->id]) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <button type="submit">Mark as {{ $task->completed ? 'not completed' : 'completed' }}!</button>
+    </form>
 
     <a href="{{ route('tasks.edit', ['task' => $task->id]) }}">Edit</a>
     <form action="{{ route('tasks.destroy', ['task' => $task->id]) }}" method="POST">
