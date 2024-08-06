878b8fe883f016e2296e174c3b62eea6887e99e6
Milton Girón <46804135+miltong261@users.noreply.github.com>
2024-08-05 21:31:05 -0600

    API backend
    * Added functionality to view, create, update, delete tasks and restore
    * Additionally, validations were added to the request

---
+++ app/Http/Requests/Task/TaskUpdateRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Task;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class TaskUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $id = $this->route('id');
+
+        return [
+            'name' => ['required', 'min:3', 'max:255', Rule::unique('tasks', 'name')->ignore($id)],
+            'description' => ['nullable', 'min:5']
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,25 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::prefix('v1')->group(function () {
+    Route::prefix('tasks')->group(function () {
+        Route::get('tasks', [\App\Http\Controllers\Api\V1\TaskController::class, 'index']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+        Route::get('/{id}', [\App\Http\Controllers\Api\V1\TaskController::class, 'show']);
+
+        Route::post('/', [\App\Http\Controllers\Api\V1\TaskController::class, 'store']);
+
+        Route::put('/{id}', [\App\Http\Controllers\Api\V1\TaskController::class, 'update']);
+
+        Route::patch('change_status/{id}', [\App\Http\Controllers\Api\V1\TaskController::class, 'changeStatus']);
+
+        Route::get('show_deleted/{id}', [\App\Http\Controllers\Api\V1\TaskController::class, 'showDeletedRecord']);
+        Route::get('show_deleted', [\App\Http\Controllers\Api\V1\TaskController::class, 'showDeletedRecords']);
+
+        Route::delete('destroy', [\App\Http\Controllers\Api\V1\TaskController::class, 'destroy']);
+        Route::delete('destroy_by_id/{id}', [\App\Http\Controllers\Api\V1\TaskController::class, 'destroyById']);
+
+        Route::patch('restore', [\App\Http\Controllers\Api\V1\TaskController::class, 'restore']);
+        Route::patch('restore/{id}', [\App\Http\Controllers\Api\V1\TaskController::class, 'restoreById']);
+    });
 });
